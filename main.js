6183975c44928517281c3dd1beff46046cdc7899
MOHAMMAD SALIM SOARES SILVA <24122313@CFP127ALUNO.LOCAL>
2025-04-03 12:25:08 -0300

    Alterações de desgin

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ export async function getListaDeContatos(numero) {
 
     return data.contacts
 }
-export async function getListaDeConversas(numero) {
+export async function getListaDeConversasAll(numero) {
     const url = `https://api-whats-1-6ulr.onrender.com/v1/whatsapp/filter/?nu=${numero}`
     
     const response = await fetch(url)
@@ -16,3 +16,35 @@ export async function getListaDeConversas(numero) {
 
     return data.contacts
 }
+export async function getListaDeConversas(numero, name) {
+    const contatos = await getListaDeConversasAll("11987876567")
+    let retorno = false
+
+    contatos.forEach(element => {
+        if(element.name = name){
+            retorno = element.messages
+        }
+    })
+    return retorno
+}
+
+async function renderContatod() {
+    const contacts = await getListaDeContatos("11987876567")
+    contacts.forEach(cardContato)
+}
+
+function cardContato(element){
+    const lista = document.getElementById("contatos")
+    const contato = document.createElement("div")
+    const titulo = document.createElement("h3")
+    titulo.textContent = element.name
+    const descricao = document.createElement("span")
+    descricao.textContent = element.description
+
+    contato.appendChild(titulo)
+    contato.appendChild(descricao)
+    lista.appendChild(contato)
+
+}
+
+renderContatod()
